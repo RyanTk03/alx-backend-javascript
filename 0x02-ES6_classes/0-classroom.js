024753c49f79d0c3777efc09715580b39cb99005
Rayane <rayanetoko@gmail.com>
2024-10-03 18:35:33 -0700

    refactor(indent): use 2 space for indentation

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -2,13 +2,13 @@
  * A class room representation
  */
 export default class ClassRoom {
-	/**
-	 * contructor of the class
-	 *
-	 * @param {Number} maxStudentsSize - max number of students in
-	 * the class.
-	 */
-	contructor(maxStudentsSize) {
-		this._maxStudentsSize = maxStudentsSize;
-	}
+  /**
+   * contructor of the class
+   *
+   * @param {Number} maxStudentsSize - max number of students in
+   * the class.
+   */
+  contructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
 }
