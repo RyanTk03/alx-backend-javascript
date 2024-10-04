730a49b309ad7eaf4c21e016d0c6c7ee032075a2
Rayane <rayanetoko@gmail.com>
2024-10-03 17:59:43 -0700

    feat(task 1): submitting task 1 to the checker

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,14 @@
+/**
+ * A class room representation
+ */
+export default class ClassRoom {
+	/**
+	 * contructor of the class
+	 *
+	 * @param {Number} maxStudentsSize - max number of students in
+	 * the class.
+	 */
+	contructor(maxStudentsSize) {
+		this._maxStudentsSize = maxStudentsSize;
+	}
+}
