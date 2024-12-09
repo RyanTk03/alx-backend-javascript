9446df862bcf2e8f340e34a7c83968d75add1b04
rayane <rayanetoko@gmail.com>
2024-12-09 21:17:36 +0000

    0x05-Node_JS_basic: update tasks 4 and 5

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,9 +1,9 @@
 const http = require('http');
 const PORT = 1245;
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('Hello ALX!');
-  re.end();
+  res.writeHead(200);
+  res.setHeader('Content-Type', 'application/json');
+  res.end('Hello ALX!');
 });
 
 app.listen(PORT);
--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -2,23 +2,22 @@ const http = require('http');
 const students = require('3-read_file_async.js');
 const PORT = 1245;
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.writeHead(200);
+  res.setHeader('Content-Type', 'text/plain');
   if (req.url === '/') {
     res.write('Hello ALX!');
   } else if (req.url === '/students') {
     students(procss.argv[2])
     .then((data) => {
-      res.write(`Number of students: ${data.students.length}\n`);
-      res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
-      res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
-      res.end();
+      res.end(`Number of students: ${data.students.length}\n` +
+      `Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n` +
+      `Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
     })
     .catch((error) => {
       res.writeHead(500);
       res.end(error.message);
     });
   }
-  re.end();
 });
 
 app.listen(PORT);
