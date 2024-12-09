63fa815c929d6ab50c671d7a34223fa35b5976c9
rayane <rayanetoko@gmail.com>
2024-12-09 13:17:41 +0000

    0x05-Node_JS_basic: add task 3

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+function countStudents(filename) {
+  return new Promise((resolve, reject) => {
+    if (!fs.existsSync(filename)) {
+      reject('Cannot load the database');
+    }
+    fs.readFile(filename, (err, data) => {
+      if (err) reject(err.message);
+      const students = data.toString().split('\r\n')
+        .map((studentLine) => studentLine.split(','))
+        .filter((studentData) => studentData.length === 4 && studentData[0] !== 'firstname');
+      const csStudents = students
+        .filter((student) => student[3] === 'CS')
+        .map((student) => student[0]);
+      const sweStudents = students
+        .filter((student) => student[3] === 'SWE')
+        .map((student) => student[0]);
+
+      console.log(`Number of students: ${students.length}`);
+      console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
+      console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
+      resolve();
+    });
+  });
+}
